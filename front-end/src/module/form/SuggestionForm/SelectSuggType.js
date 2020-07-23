61007d18a460f70ad3db54d728c2ecd6599593bb
Billie Zhang <billiecoder@gmail.com>
2020-07-23 15:20:43 +0800

    change sugg types

--- front-end/src/module/form/SuggestionForm/SelectSuggType.js
+++ front-end/src/module/form/SuggestionForm/SelectSuggType.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Radio } from 'antd'
+import { Radio, InputNumber, Input } from 'antd'
 import styled from 'styled-components'
 import I18N from '@/I18N'
 import { SUGGESTION_TYPE } from '@/constant'
@@ -18,27 +18,53 @@ class SelectSuggType extends Component {
     const value = props.initialValue
     this.state = {
       type: (value && value.type) || '1',
-      newSecretary: (value && value.secretary) || ''
+      newSecretary: (value && value.newSecretary) || null,
+      proposalNum: (value && value.proposalNum) || null,
+      newOwner: (value && value.newOwner) || null
     }
   }
 
-  changeValue(value) {
-    const { onChange, callback } = this.props
-    onChange(value)
-    callback('type')
+  changeValue() {
+    const { onChange } = this.props
+    const { type, newOwner, newSecretary, proposalNum } = this.state
+    let data
+    switch (type) {
+      case CHANGE_PROPOSAL_OWNER:
+        data = { type, newOwner, proposalNum }
+        break
+      case CHANGE_SECRETARY:
+        data = { type, newSecretary }
+        break
+      case TERMINATE_PROPOSAL:
+        data = { type, proposalNum }
+        break
+      default:
+        data = { type }
+        break
+    }
+    onChange(data)
   }
 
-  handleChange = (e) => {
-    this.setState({
-      type: e.target.value
+  handleChange = (e, field) => {
+    this.setState({ [field]: e.target.value }, () => {
+      this.changeValue()
+    })
+  }
+
+  handleNumChange = (value) => {
+    this.setState({ proposalNum: value }, () => {
+      this.changeValue()
     })
   }
 
   render() {
-    const { type } = this.state
+    const { type, newOwner, newSecretary, proposalNum } = this.state
     return (
       <Wrap>
-        <Radio.Group onChange={this.handleChange} value={this.state.type}>
+        <Radio.Group
+          onChange={(e) => this.handleChange(e, 'type')}
+          value={type}
+        >
           <Radio value={NEW_MOTION}>
             {I18N.get('suggestion.form.type.newMotion')}
           </Radio>
@@ -58,9 +84,44 @@ class SelectSuggType extends Component {
             {I18N.get('suggestion.form.type.anythingElse')}
           </Radio>
         </Radio.Group>
-        {type === CHANGE_PROPOSAL_OWNER && <div>change owner</div>}
-        {type === CHANGE_SECRETARY && <div>change secretary</div>}
-        {type === TERMINATE_PROPOSAL && <div>terminate proposal</div>}
+        {type === CHANGE_PROPOSAL_OWNER && (
+          <div>
+            <div>
+              proposal new owner DID:{' '}
+              <Input
+                onChange={(e) => this.handleChange(e, 'newOwner')}
+                value={newOwner}
+              />
+            </div>
+            <div>
+              Proposal Number{' '}
+              <InputNumber
+                onChange={this.handleNumChange}
+                value={proposalNum}
+                min={0}
+              />
+            </div>
+          </div>
+        )}
+        {type === CHANGE_SECRETARY && (
+          <div>
+            new secretary DID:{' '}
+            <Input
+              onChange={(e) => this.handleChange(e, 'newSecretary')}
+              value={newSecretary}
+            />
+          </div>
+        )}
+        {type === TERMINATE_PROPOSAL && (
+          <div>
+            Proposal Number{' '}
+            <InputNumber
+              onChange={this.handleNumChange}
+              value={proposalNum}
+              min={0}
+            />
+          </div>
+        )}
       </Wrap>
     )
   }
