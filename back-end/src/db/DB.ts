4bd7e31b2fc1358e671c755fb57c9be8a627ccca
Billie Zhang <billiecoder@gmail.com>
2020-08-27 17:19:39 +0800

    add cvote tracking message model

--- back-end/src/db/DB.ts
+++ back-end/src/db/DB.ts
@@ -26,7 +26,7 @@ import Secretariat from './Secretariat'
 import Ela_Transaction from './Ela_Transaction'
 import Config from './Config'
 import CVote_Vote_History from './CVote_Vote_History'
-
+import CVote_Tracking_Message from './CVote_Tracking_Message'
 import Log from './Log'
 
 import { utilCrypto } from '../utility'
@@ -54,7 +54,7 @@ export default class {
 
     this.connection.on('disconnected', this.handleUnexpectedDisconnect)
 
-    this.connection.on('reconnected', function() {
+    this.connection.on('reconnected', function () {
       console.log('MongoDB reconnected!')
     })
 
@@ -127,6 +127,7 @@ export default class {
     this.db.Ela_Transaction = new Ela_Transaction(db)
     this.db.Config = new Config(db)
     this.db.CVote_Vote_History = new CVote_Vote_History(db)
+    this.db.CVote_Tracking_Message = new CVote_Tracking_Message(db)
   }
 
   public getModel(name: string) {
