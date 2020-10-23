82b39eba603fd7a663949f0fb5ca5a78245ad742
Andy Lian <andy.lodestar00@gmail.com>
2020-10-23 09:59:54 +0800

    fixed relevance no save

--- back-end/src/service/SuggestionService.ts
+++ back-end/src/service/SuggestionService.ts
@@ -977,6 +977,21 @@ export default class extends Base {
     if (doc.budgetAmount) {
       doc.budgetAmount = Big(doc.budgetAmount).toFixed()
     }
+    // compatible with old relevance
+    if (doc.relevance) {
+      let relevanceStr = ''
+      _.forEach(doc.relevance[0], (v, k) => {
+        if (k === '0') {
+          _.forEach(doc.relevance[0], (v) => {
+            relevanceStr += v
+          })
+        }
+        return
+      })
+      if (!_.isEmpty(relevanceStr)) {
+        doc.relevance = relevanceStr
+      }
+    }
 
     if (doc && _.isEmpty(doc.comments)) return doc
 
