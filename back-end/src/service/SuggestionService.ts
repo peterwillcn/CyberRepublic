1e0f9db130aca26db0d4988a37dbe8ac511ad863
Andy Lian <andy.lodestar00@gmail.com>
2020-11-23 11:11:25 +0800

    optimize suggestion detail loading speed

--- back-end/src/service/SuggestionService.ts
+++ back-end/src/service/SuggestionService.ts
@@ -1026,13 +1026,13 @@ export default class extends Base {
         for (const thread of comment) {
           await model.getDBInstance().populate(thread, {
             path: 'createdBy',
-            select: `${constant.DB_SELECTED_FIELDS.USER.NAME_EMAIL_DID} profile.avatar`
+            select: `${constant.DB_SELECTED_FIELDS.USER.NAME_EMAIL_DID}`
           })
           if (thread.childComment.length > 0) {
             for (const child of thread.childComment) {
               await model.getDBInstance().populate(child, {
                 path: 'createdBy',
-                select: `${constant.DB_SELECTED_FIELDS.USER.NAME_EMAIL_DID} profile.avatar`
+                select: `profile.avatar profile.firstName profile.lastName`
               })
             }
           }
