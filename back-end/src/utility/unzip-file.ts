1b80ff30cb0924dac765dc02fe3a82870315d6d6
Alix <>
2021-12-15 19:12:54 +0800

    use decompress to extract zip file

--- back-end/src/utility/unzip-file.ts
+++ back-end/src/utility/unzip-file.ts
@@ -1,43 +1,18 @@
-import * as fs from 'fs'
-import * as path from 'path'
-const extract = require('extract-zip')
-
-const deleteFolderRecursive = function (directoryPath) {
-  if (fs.existsSync(directoryPath)) {
-    fs.readdirSync(directoryPath).forEach((file) => {
-      const curPath = path.join(directoryPath, file)
-      if (fs.lstatSync(curPath).isDirectory()) {
-        deleteFolderRecursive(curPath)
-      } else {
-        fs.unlinkSync(curPath)
-      }
-    })
-    fs.rmdirSync(directoryPath)
-  }
-}
-
-async function extractZip(source: string, target: string) {
-  try {
-    await extract(source, { dir: target })
-    console.log('Extraction complete')
-  } catch (err) {
-    console.log('Oops: extractZip failed', err)
-  }
-}
+const decompress = require('decompress')
 
 export const unzipFile = async (opinionData: string) => {
   try {
     const bytes = Buffer.from(opinionData, 'hex')
-    const zipFilePath = `${__dirname}/opinion.zip`
-    fs.writeFileSync(zipFilePath, bytes)
-    const dir = `${__dirname}/opinion`
-    await extractZip(zipFilePath, dir)
-    fs.unlinkSync(zipFilePath)
-    const opinionFilePath = `${__dirname}/opinion/opinion.json`
-    const text = fs.readFileSync(opinionFilePath, 'utf-8')
-    const opinion = JSON.parse(text).content
-    deleteFolderRecursive(dir)
-    return opinion
+    const files = await decompress(bytes)
+    if (files.length > 0) {
+      const file = files.find((el: any) => el.path === 'opinion.json')
+      console.log(`file...`, file)
+      if (file && file.data) {
+        const text = file.data.toString()
+        const opinion = JSON.parse(text).content
+        return opinion
+      }
+    }
   } catch (err) {
     console.log(`unzipFile err...`, err)
   }
