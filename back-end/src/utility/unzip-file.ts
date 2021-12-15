135f0da4087f48d8a62320126ecec7e48c8118c5
Alix <>
2021-12-15 18:42:33 +0800

    wrap extract function

--- back-end/src/utility/unzip-file.ts
+++ back-end/src/utility/unzip-file.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs'
 import * as path from 'path'
-import * as extract from 'extract-zip'
+const extract = require('extract-zip')
 
 const deleteFolderRecursive = function (directoryPath) {
   if (fs.existsSync(directoryPath)) {
@@ -16,13 +16,22 @@ const deleteFolderRecursive = function (directoryPath) {
   }
 }
 
-export async function unzipFile(opinionData: string) {
+async function extractZip(source: string, target: string) {
+  try {
+    await extract(source, { dir: target })
+    console.log('Extraction complete')
+  } catch (err) {
+    console.log('Oops: extractZip failed', err)
+  }
+}
+
+export const unzipFile = async (opinionData: string) => {
   try {
     const bytes = Buffer.from(opinionData, 'hex')
     const zipFilePath = `${__dirname}/opinion.zip`
     fs.writeFileSync(zipFilePath, bytes)
     const dir = `${__dirname}/opinion`
-    await extract(zipFilePath, { dir })
+    await extractZip(zipFilePath, dir)
     fs.unlinkSync(zipFilePath)
     const opinionFilePath = `${__dirname}/opinion/opinion.json`
     const text = fs.readFileSync(opinionFilePath, 'utf-8')
